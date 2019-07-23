cfc07d54a913a63d29ffa4b9a7b9c7d6d5c580c8
Alex Harasty <alex.harasty@thinkingwithtools.com>
2019-07-23 15:36:18 -0400

    add offset for headerheight scroll

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 $(document).foundation();
 // need to reset to set scroll offset
+$(document).ready(function() {
+    var _headerHeightOffset = $('header').height();
+    // preserve header offset
+    function scrollToEl(scrollToSelector) {
+        if (!scrollToSelector) {
+            return;
+        }
+        var offset = $(scrollToSelector).offset();
+        if (offset && offset.top) {
+            $('html, body')
+            .animate(
+                {
+                    scrollTop: offset.top - _headerHeightOffset 
+                }
+            );
+        }
+    }
+    $('header a').click(function(event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        scrollToEl(href);
+    });
+});
